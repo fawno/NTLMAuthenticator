522ff1e1148ba9b939fd2d7ecb8c6c58a3875074
Fernando Herrero <alphp@users.noreply.github.com>
2022-04-26 01:28:49 +0200

    Readme, requirements

---
+++ README.md
@@ -0,0 +1,123 @@
+# NTLM Authenticator for CakePHP 4 Authentication plugin
+
+This plugin provides an NTLM Authenticator for CakePHP 4 authentication plugin.
+
+# Table of contents
+- [Requirements](#requirements)
+- [Installation](#installation)
+- [Configuration](#configuration)
+  - [Apache with SSPI NTLM based authentication module (mod_authn_ntlm)](#apache-with-sspi-ntlm-based-authentication-module-mod_authn_ntlm)
+  - [NTLMAuthenticator](#ntlmauthenticator)
+
+## Requirements
+
+- PHP >= 7.2.0
+- Apache 2.4 SSPI NTLM based authentication module ([mod_authn_ntlm](https://github.com/TQsoft-GmbH/mod_authn_ntlm))
+- CakePHP >= 4.3.0
+- [CakePHP Authentication](https://book.cakephp.org/authentication/2/en/index.html) >= 2.0
+
+Optional:
+- ext-ldap ([LDAP php extension](https://www.php.net/manual/en/book.ldap.php))
+
+[TOC](#table-of-contents)
+
+## Installation
+
+Install this plugin into your application using [composer](https://getcomposer.org):
+
+- Add fawno/ntlm-authentication package to your project:
+  ```bash
+    composer require fawno/ntlm-authentication
+  ```
+- Load the NTLMAuthenticator in your Application.php:
+  ```php
+  use Fawno\NTLM\Authenticator\NTLMAuthenticator;
+  ```
+- Load the NTLMAuthenticator in your Authentication Service (Application.php):
+  ```php
+  // Load the authenticators. Session should be first.
+  $service->loadAuthenticator('Authentication.Session');
+
+  $service->loadAuthenticator(NTLMAuthenticator::class, [
+      'domains' => [],
+  ]);
+  ```
+
+[TOC](#table-of-contents)
+
+## Configuration
+
+`exampledomain` short domain name
+`example.com` full domain name
+
+### Apache with SSPI NTLM based authentication module ([mod_authn_ntlm](https://github.com/TQsoft-GmbH/mod_authn_ntlm))
+
+Only routes with /login are authenticated with NTLM
+`webroot\.htaccess`:
+```aconf
+<If "%{THE_REQUEST} =~ m#GET .*/login(\?.*)? HTTP.*#">
+	AuthName "Example App"
+	AuthType SSPI
+	NTLMAuth On
+	NTLMAuthoritative On
+	NTLMDomain exampledomain
+	NTLMOmitDomain Off     # keep domain name in userid string
+	NTLMOfferBasic On      # let non-IE clients authenticate
+	NTLMBasicPreferred Off # should basic authentication have higher priority
+	NTLMUsernameCase lower
+	Require valid-user
+</If>
+<Else>
+	AuthType None
+	Require all granted
+</Else>
+
+#Order allow,deny
+#Allow from 192.168.0.0/16
+Satisfy all
+```
+
+[TOC](#table-of-contents)
+
+### NTLMAuthenticator
+
+NTLM Authenticator can query through LDAP for user membership. This information is stored in the session and can be used for authorization (ACL).
+
+```php
+$service->loadAuthenticator(NTLMAuthenticator::class, [
+    'domains' => [
+        'exampledomain' => [
+            'ldap' => [
+                'srv' => 'active-directory.example.com',
+                'user' => base64_encode('user@example.com'),
+                'pass' => base64_encode('UserPassword'),
+                'dn' => 'OU=Departaments, DC=example, DC=com',
+                'dn_users' => 'CN=Users, DC=example, DC=com',
+            ],
+            'config' => [
+                'some_key' => 'some_data',
+            ],
+        ],
+        'exampledomain2' => [
+            'ldap' => [
+                'srv' => 'active-directory.example2.com',
+                'user' => base64_encode('user@example2.com'),
+                'pass' => base64_encode('UserPassword2'),
+                'dn' => 'OU=Departaments, DC=example2, DC=com',
+                'dn_users' => 'CN=Users, DC=example2, DC=com',
+            ],
+            'config' => [
+                'some_key' => 'some_data',
+            ],
+        ],
+    ],
+]);
+```
+The configured credentials should have query-only access to the LDAP service and no other privileges within the domain.
+
+`config` array is optional data can be stored in session auth data.
+It allows configuring the logo of the organization and other data common to the users of a domain that the application needs to use.
+
+The application does not have any access to validated user passwords, all NTLM authentication is negotiated between the Apache server and the browser.
+
+[TOC](#table-of-contents)
--- src/Authenticator/NTLMAuthenticator.php
+++ src/Authenticator/NTLMAuthenticator.php
@@ -1,157 +1,157 @@
 <?php
-	declare(strict_types=1);
+    declare(strict_types=1);
 
-	namespace Fawno\NTLM\Authenticator;
+    namespace Fawno\NTLM\Authenticator;
 
-	use Authentication\Authenticator\AbstractAuthenticator;
-	use Authentication\Authenticator\PersistenceInterface;
-	use Authentication\Authenticator\Result;
-	use Authentication\Authenticator\ResultInterface;
-	use Psr\Http\Message\ResponseInterface;
-	use Psr\Http\Message\ServerRequestInterface;
+    use Authentication\Authenticator\AbstractAuthenticator;
+    use Authentication\Authenticator\PersistenceInterface;
+    use Authentication\Authenticator\Result;
+    use Authentication\Authenticator\ResultInterface;
+    use Psr\Http\Message\ResponseInterface;
+    use Psr\Http\Message\ServerRequestInterface;
 
-	class NTLMAuthenticator extends AbstractAuthenticator implements PersistenceInterface {
-    /**
-     * Default config for this object.
-     * - `sessionKey` Session key.
-		 * - `domains` Array with domain(s) config
-		 *
-     * @var array
-     */
-    protected $_defaultConfig = [
-			'sessionKey' => 'Auth',
-			'domains' => [],
-		];
-
-    /**
-     * Authenticate a user using NTLM server data.
-		 * Server params:
-		 * - `AUTH_TYPE` NTLM
-		 * - `REMOTE_USER` domain\username
-     *
-     * @param \Psr\Http\Message\ServerRequestInterface $request The request to authenticate with.
-     * @return \Authentication\Authenticator\ResultInterface
-     */
-		public function authenticate (ServerRequestInterface $request) : ResultInterface {
-			$user = false;
-
-			$server = $request->getServerParams();
-			$auth_type = $server['AUTH_TYPE'] ?? '';
-			$remote_user = $server['REMOTE_USER'] ?? '';
-
-			if ($auth_type !== 'NTLM') {
-				return new Result(null, Result::FAILURE_CREDENTIALS_MISSING);
-			}
-
-			$user = $this->get_user_data($remote_user);
-
-			if (empty($user)) {
-				return new Result(null, Result::FAILURE_IDENTITY_NOT_FOUND, $this->_identifier->getErrors());
-			}
-
-			return new Result($user, Result::SUCCESS);
-		}
-
-		protected function get_user_data (string $auth_user) {
-			$user = [
-				'domain' => dirname($auth_user),
-				'username' => basename($auth_user),
-				'displayname' => dirname($auth_user),
-				'dn' => [],
-				'memberof' => [],
-			];
-
-			$domain = $this->getConfig('domains.' . $user['domain']);
-			if ($domain) {
-				$ldap = ldap_connect($domain['ldap']['srv']);
-				ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
-				ldap_bind($ldap, base64_decode($domain['ldap']['user']), base64_decode($domain['ldap']['pass']));
-
-				$attributes = ['displayname', 'samaccountname', 'memberof'];
-				$filter = '(samaccountname=' . $user['username'] . ')';
-				$result = ldap_search($ldap, $domain['ldap']['dn'], $filter, $attributes);
-
-				$entries = ldap_get_entries($ldap, $result);
-				if (!$entries['count']) {
-					$result = ldap_search($ldap, $domain['ldap']['dn_users'], $filter, $attributes);
-					$entries = ldap_get_entries($ldap, $result);
-				}
-				if (!$entries['count']) {
-					return false;
-				}
-
-				ldap_unbind($ldap);
-
-				$user['displayname'] = $entries[0]['displayname'][0] ?? $user['username'];
-
-				if (isset($entries[0]['dn'])) {
-					foreach(explode(',', $entries[0]['dn']) as $dn_entry) {
-						list($dn_key, $dn_value) = explode('=', $dn_entry);
-						if ($dn_key == 'OU') {
-							$user['dn'][$dn_key][$dn_value] = $dn_value;
-						} else {
-							$user['dn'][$dn_key][] = $dn_value;
-						}
-					}
-				}
-
-				if (isset($entries[0]['memberof'])) {
-					foreach ($entries[0]['memberof'] as $key => $memberof) {
-						if (is_int($key)) {
-							foreach(explode(',', $memberof) as $memberof_entry) {
-								list($memberof_key, $memberof_value) = explode('=', $memberof_entry . '=');
-								if ($memberof_key == 'CN') $user['memberof'][$memberof_value] = $memberof_value;
-							}
-						}
-					}
-				}
-
-				if (is_array($user['displayname'])) {
-					if (!empty($user['dn']['CN'][0])) {
-						$user['displayname'] = $user['dn']['CN'][0];
-					} else {
-						$user['displayname'] = implode('/', $user['username']);
-					}
-				}
-
-				$user['config'] = $domain['config'] ?? null;
-			}
-
-			return $user;
-		}
-
-		/**
-     * @inheritDoc
-     */
-		public function persistIdentity (ServerRequestInterface $request, ResponseInterface $response, $identity) : array {
-			$sessionKey = $this->getConfig('sessionKey');
-			/** @var \Cake\Http\Session $session */
-			$session = $request->getAttribute('session');
-
-			if (!$session->check($sessionKey)) {
-				$session->renew();
-				$session->write($sessionKey, $identity);
-			}
-
-			return [
-				'request' => $request,
-				'response' => $response,
-			];
-		}
-
-    /**
-     * @inheritDoc
-     */
-    public function clearIdentity (ServerRequestInterface $request, ResponseInterface $response) : array {
-        $sessionKey = $this->getConfig('sessionKey');
-        /** @var \Cake\Http\Session $session */
-        $session = $request->getAttribute('session');
-        $session->delete($sessionKey);
-        $session->renew();
-
-        return [
-					'request' => $request->withoutAttribute($this->getConfig('identityAttribute')),
-					'response' => $response,
+    class NTLMAuthenticator extends AbstractAuthenticator implements PersistenceInterface {
+        /**
+         * Default config for this object.
+         * - `sessionKey` Session key.
+	     * - `domains` Array with domain(s) config
+	     *
+         * @var array
+         */
+        protected $_defaultConfig = [
+            'sessionKey' => 'Auth',
+            'domains' => [],
         ];
+
+        /**
+         * Authenticate a user using NTLM server data.
+	     * Server params:
+	     * - `AUTH_TYPE` NTLM
+	     * - `REMOTE_USER` domain\username
+         *
+         * @param \Psr\Http\Message\ServerRequestInterface $request The request to authenticate with.
+         * @return \Authentication\Authenticator\ResultInterface
+         */
+        public function authenticate (ServerRequestInterface $request) : ResultInterface {
+            $user = false;
+
+            $server = $request->getServerParams();
+            $auth_type = $server['AUTH_TYPE'] ?? '';
+            $remote_user = $server['REMOTE_USER'] ?? '';
+
+            if ($auth_type !== 'NTLM') {
+                return new Result(null, Result::FAILURE_CREDENTIALS_MISSING);
+            }
+
+            $user = $this->get_user_data($remote_user);
+
+            if (empty($user)) {
+                return new Result(null, Result::FAILURE_IDENTITY_NOT_FOUND, $this->_identifier->getErrors());
+            }
+
+            return new Result($user, Result::SUCCESS);
+        }
+
+        protected function get_user_data (string $auth_user) {
+            $user = [
+                'domain' => dirname($auth_user),
+                'username' => basename($auth_user),
+                'displayname' => dirname($auth_user),
+                'dn' => [],
+                'memberof' => [],
+            ];
+
+            $domain = $this->getConfig('domains.' . $user['domain']);
+            if ($domain) {
+                $ldap = ldap_connect($domain['ldap']['srv']);
+                ldap_set_option($ldap, LDAP_OPT_PROTOCOL_VERSION, 3);
+                ldap_bind($ldap, base64_decode($domain['ldap']['user']), base64_decode($domain['ldap']['pass']));
+
+                $attributes = ['displayname', 'samaccountname', 'memberof'];
+                $filter = '(samaccountname=' . $user['username'] . ')';
+                $result = ldap_search($ldap, $domain['ldap']['dn'], $filter, $attributes);
+
+                $entries = ldap_get_entries($ldap, $result);
+                if (!$entries['count']) {
+                    $result = ldap_search($ldap, $domain['ldap']['dn_users'], $filter, $attributes);
+                    $entries = ldap_get_entries($ldap, $result);
+                }
+                if (!$entries['count']) {
+                    return false;
+                }
+
+                ldap_unbind($ldap);
+
+                $user['displayname'] = $entries[0]['displayname'][0] ?? $user['username'];
+
+                if (isset($entries[0]['dn'])) {
+                    foreach(explode(',', $entries[0]['dn']) as $dn_entry) {
+                        list($dn_key, $dn_value) = explode('=', $dn_entry);
+                        if ($dn_key == 'OU') {
+                            $user['dn'][$dn_key][$dn_value] = $dn_value;
+                        } else {
+                            $user['dn'][$dn_key][] = $dn_value;
+                        }
+                    }
+                }
+
+                if (isset($entries[0]['memberof'])) {
+                    foreach ($entries[0]['memberof'] as $key => $memberof) {
+                        if (is_int($key)) {
+                            foreach(explode(',', $memberof) as $memberof_entry) {
+                                list($memberof_key, $memberof_value) = explode('=', $memberof_entry . '=');
+                                if ($memberof_key == 'CN') $user['memberof'][$memberof_value] = $memberof_value;
+                            }
+                        }
+                    }
+                }
+
+                if (is_array($user['displayname'])) {
+                    if (!empty($user['dn']['CN'][0])) {
+                        $user['displayname'] = $user['dn']['CN'][0];
+                    } else {
+                        $user['displayname'] = implode('/', $user['username']);
+                    }
+                }
+
+                $user['config'] = $domain['config'] ?? null;
+            }
+
+            return $user;
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function persistIdentity (ServerRequestInterface $request, ResponseInterface $response, $identity) : array {
+            $sessionKey = $this->getConfig('sessionKey');
+            /** @var \Cake\Http\Session $session */
+            $session = $request->getAttribute('session');
+
+            if (!$session->check($sessionKey)) {
+                $session->renew();
+                $session->write($sessionKey, $identity);
+            }
+
+            return [
+                'request' => $request,
+                'response' => $response,
+            ];
+        }
+
+        /**
+         * @inheritDoc
+         */
+        public function clearIdentity (ServerRequestInterface $request, ResponseInterface $response) : array {
+            $sessionKey = $this->getConfig('sessionKey');
+            /** @var \Cake\Http\Session $session */
+            $session = $request->getAttribute('session');
+            $session->delete($sessionKey);
+            $session->renew();
+
+            return [
+                        'request' => $request->withoutAttribute($this->getConfig('identityAttribute')),
+                        'response' => $response,
+            ];
+        }
     }
-	}
