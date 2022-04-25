5557c03946fe5a49b5dd334c6702d8410d08cb05
Fernando Herrero <alphp@users.noreply.github.com>
2022-04-25 23:59:43 +0200

    Core

---
+++ src/Plugin.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE file
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright   Copyright (c) Fernando Herrero (https://github.com/alphp)
+ * @license     https://opensource.org/licenses/mit-license.php MIT License
+ * @link        https://github.com/fawno/NTLMAuthenticator
+ */
+namespace Fawno\NTLM;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class.
+ */
+class Plugin extends BasePlugin {
+}
